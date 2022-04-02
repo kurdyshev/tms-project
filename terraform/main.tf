771ee82db933d6e470af43a21e03077454e7b607
Vasilii Kurdyshev <kurdyshev@gmail.com>
2022-04-02 20:54:24 +0300

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">=1.1.7"
+    backend "s3" {
+      bucket = "tf-sta"
+      key = "terraform.tfstate"
+      region = "us-east-1"
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
